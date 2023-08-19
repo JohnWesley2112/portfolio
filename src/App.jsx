7a6295d3c5725174ff68ca8ba51487a48372e228
JohnWesley2112 <johnwesley14307@gmail.com>
2023-08-19 21:14:08 +0530

    test edited slightly

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import './App.css';
 import Header from './components/pages/Header';
-// import Home from './components/pages/Home.jsx';
+import Footer from './components/pages/Footer';
+import Home from './components/pages/Home';
 // import About from './components/pages/About';
 // import Projects from './components/pages/Projects';
-// import Contacts from './components/pages/Contacts';
 // import { Routes, Route } from 'react-router-dom';
 import { useEffect } from 'react';
 // Animate On Scroll 
 import AOS from "aos";
 import "aos/dist/aos.css";
-import Footer from './components/pages/Footer';
-import Home from './components/pages/Home';
 
 function App() {
 
