b22d1bd14b0c9c2f254b165cc3967c4ead9c3aef
JohnWesley2112 <johnwesley14307@gmail.com>
2025-09-21 22:50:51 +0530

    Remove old CRA site

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders John Wesley', () => {
-  render(<App />);
-  const name = screen.getByText(/John Wesley/i);
-  expect(name).toBeInTheDocument();
-});
